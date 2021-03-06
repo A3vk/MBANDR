d3befaf500922fde3fea1bed2d4c7d2221e729be
Kevin Treffers <kevintreffers@msn.com>
2021-03-06 20:20:47 +0100

    changed fragment and xml for settings

--- app/src/main/java/com/example/mypokedexapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/mypokedexapp/ui/settings/SettingsFragment.kt
@@ -2,31 +2,35 @@ package com.example.mypokedexapp.ui.settings
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.lifecycle.Observer
+import androidx.preference.PreferenceFragmentCompat
 import com.example.mypokedexapp.R
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
     private lateinit var settingsViewModel: SettingsViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        settingsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_settings, container, false)
-        val textView: TextView = root.findViewById(R.id.text_settings)
-        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        settingsViewModel =
+//            ViewModelProvider(this).get(SettingsViewModel::class.java)
+//        val root = inflater.inflate(R.layout.fragment_settings, container, false)
+//        val textView: TextView = root.findViewById(R.id.text_settings)
+//        settingsViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+//        return root
+//    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.layout.fragment_settings, rootKey)
     }
 
 }
