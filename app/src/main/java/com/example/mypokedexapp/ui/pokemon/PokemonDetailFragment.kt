7bb9b81ccd272b1b8963d01ee32d13e63adb44b0
Lavyra1 <kevintreffers@msn.com>
2021-03-07 15:54:14 +0100

    in between
    vertalingen inbetween

--- app/src/main/java/com/example/mypokedexapp/ui/pokemon/PokemonDetailFragment.kt
+++ app/src/main/java/com/example/mypokedexapp/ui/pokemon/PokemonDetailFragment.kt
@@ -1,8 +1,7 @@
 package com.example.mypokedexapp.ui.pokemon
 
-import android.content.res.ColorStateList
+import android.content.SharedPreferences
 import android.graphics.Color
-import android.graphics.ColorFilter
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -55,7 +54,7 @@ class PokemonDetailFragment : Fragment(){
                 if (index < pokemon.types.count()) {
                     val textView = view as TextView
                     val type = pokemon.types[index]
-                    textView.text = type.name.capitalize(Locale.ROOT)
+                    textView.text = getPokemonTypeName(type.name, sharedPref).capitalize(Locale.ROOT)
                     textView.setBackgroundColor(Color.parseColor(type.color))
                 }
             }
@@ -66,7 +65,7 @@ class PokemonDetailFragment : Fragment(){
                 val value = linearLayout[1] as TextView
                 val valueBar = linearLayout[2] as ProgressBar
                 val stat = pokemon.stats[index]
-                title.text = stat.name
+                title.text = resources.getString(resources.getIdentifier("hp", "string", context?.packageName))
                 value.text = stat.value.toString()
                 valueBar.progress = (stat.value / 255.0 * 100).roundToInt()
                 val colorFilter=  BlendModeColorFilterCompat.createBlendModeColorFilterCompat(Color.parseColor(color), BlendModeCompat.SRC_ATOP)
@@ -75,5 +74,93 @@ class PokemonDetailFragment : Fragment(){
         })
 
         return root
+    }
+
+//    private fun getPokemonStatNameEn(stat: String) :String{
+//        return when (stat){
+//            "hp" -> resources.getString(R.string.stat_hp_en)
+//            "attack" -> resources.getString(R.string.stat_attack_en)
+//            "defense" -> resources.getString(R.string.stat_defense_en)
+//            "special-attack" -> resources.getString(R.string.stat_special_attack_en)
+//            "special-defense" -> resources.getString(R.string.stat_special_defense_en)
+//            "speed" -> resources.getString(R.string.stat_speed_en)
+//            else -> ""
+//        }
+//    }
+
+    private fun getPokemonStatNameNl(stat: String) :String{
+        return when (stat){
+            "hp" -> resources.getString(R.string.stat_hp_nl)
+            "attack" -> resources.getString(R.string.stat_attack_nl)
+            "defense" -> resources.getString(R.string.stat_defense_nl)
+            "special-attack" -> resources.getString(R.string.stat_special_attack_nl)
+            "special-defense" -> resources.getString(R.string.stat_special_defense_nl)
+            "speed" -> resources.getString(R.string.stat_speed_nl)
+            else -> ""
+        }
+    }
+
+    private fun getPokemonStatName(stat: String, sharedPreferences: SharedPreferences): String{
+        return when (sharedPreferences.getString("language_preference", "EN")){
+//            "EN" -> return getPokemonStatNameEn(stat)
+            "NL" -> return getPokemonStatNameNl(stat)
+            else -> ""
+        }
+    }
+
+    private fun getPokemonTypeNameEn(stat: String) :String{
+        return when(stat){
+            "water" -> resources.getString(R.string.type_water_en)
+            "fire" -> resources.getString(R.string.type_fire_en)
+            "grass" -> resources.getString(R.string.type_grass_en)
+            "ground" -> resources.getString(R.string.type_ground_en)
+            "rock" -> resources.getString(R.string.type_rock_en)
+            "steel" -> resources.getString(R.string.type_steel_en)
+            "ice" -> resources.getString(R.string.type_ice_en)
+            "electric" -> resources.getString(R.string.type_electric_en)
+            "dragon" -> resources.getString(R.string.type_dragon_en)
+            "ghost" -> resources.getString(R.string.type_ghost_en)
+            "psychic" -> resources.getString(R.string.type_psychic_en)
+            "normal" -> resources.getString(R.string.type_normal_en)
+            "fighting" -> resources.getString(R.string.type_fighting_en)
+            "poison" -> resources.getString(R.string.type_poison_en)
+            "bug" -> resources.getString(R.string.type_bug_en)
+            "flying" -> resources.getString(R.string.type_flying_en)
+            "dark" -> resources.getString(R.string.type_dark_en)
+            "fairy" -> resources.getString(R.string.type_fairy_en)
+            else -> ""
+        }
+    }
+
+    private fun getPokemonTypeNameNl(stat: String) :String{
+        return when(stat){
+            "water" -> resources.getString(R.string.type_water_nl)
+            "fire" -> resources.getString(R.string.type_fire_nl)
+            "grass" -> resources.getString(R.string.type_grass_nl)
+            "ground" -> resources.getString(R.string.type_ground_nl)
+            "rock" -> resources.getString(R.string.type_rock_nl)
+            "steel" -> resources.getString(R.string.type_steel_nl)
+            "ice" -> resources.getString(R.string.type_ice_nl)
+            "electric" -> resources.getString(R.string.type_electric_nl)
+            "dragon" -> resources.getString(R.string.type_dragon_nl)
+            "ghost" -> resources.getString(R.string.type_ghost_nl)
+            "psychic" -> resources.getString(R.string.type_psychic_nl)
+            "normal" -> resources.getString(R.string.type_normal_nl)
+            "fighting" -> resources.getString(R.string.type_fighting_nl)
+            "poison" -> resources.getString(R.string.type_poison_nl)
+            "bug" -> resources.getString(R.string.type_bug_nl)
+            "flying" -> resources.getString(R.string.type_flying_nl)
+            "dark" -> resources.getString(R.string.type_dark_nl)
+            "fairy" -> resources.getString(R.string.type_fairy_nl)
+            else -> ""
+        }
+    }
+
+    private fun getPokemonTypeName(stat: String, sharedPreferences: SharedPreferences) :String{
+        return when (sharedPreferences.getString("language_preference", "EN")){
+            "EN" -> return getPokemonTypeNameEn(stat)
+            "NL" -> return getPokemonTypeNameNl(stat)
+            else -> ""
+        }
     }
 }
